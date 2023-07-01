d2d0e9fa96e58b68f3115ffa07e6be8a220ebeff
d-ohiffy <kulsum13o3.khan@gmail.com>
2023-07-01 10:15:39 +0530

    first commit

---
+++ todo/src/Components/Form.js
@@ -0,0 +1,33 @@
+import styles from "../style.module.css";
+import shortid from 'shortid';
+
+const Form = ({todo, setTodo, todoList, setTodoList})=> {
+    const handleChange= (event)=>{
+        setTodo(event.target.value);
+    };
+    const handleSubmit = (event)=>{
+        event.preventDefault();
+        setTodoList([...todoList, {name:todo, id:shortid.generate()}]);
+        // the dots is a spread operator, it spreads all the array elements 
+        console.log(todoList);
+        setTodo("");
+    };
+    return(
+        <div className={styles.todoform}>
+            <form onSubmit={handleSubmit}>
+                <input 
+                value={todo}
+                onChange = {handleChange}
+                className={styles.todoinput}
+                placeholder="Add todo item" ></input>
+                <button
+                type="submit"
+                className={styles.todobutton}
+                > Add </button>
+            </form>
+        </div>
+
+    );
+}
+
+export default Form;
